d31e55f133ddc012d2983e7f73240cae04d9dca6
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-05-04 03:40:11 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -21,7 +21,11 @@ def trim_title(title: str):
             else:
                 title = f"[AniDL] {titler.strip()} - {episode.strip()} [Web ~ {source.strip()}][720p x265 10Bit][Dual-Audio ~ Opus].mkv"
         else:
-            title = f"[AniDL] {at.strip()} - {episode.strip()} [Web ~ {source.strip()}][720p x265 10Bit][Dual-Audio ~ Opus].mkv"
+            if source=="HIDI":
+                source = source.replace("HIDI", "HIDIVE")
+                title = f"[AniDL] {at.strip()} - {episode.strip()} [Web ~ {source.strip()}][720p x265 10Bit][Dual-Audio ~ Opus].mkv"
+            else:
+                title = f"[AniDL] {at.strip()} - {episode.strip()} [Web ~ {source.strip()}][720p x265 10Bit][Dual-Audio ~ Opus].mkv"
     return title
 
 def multi_sub(title: str):
